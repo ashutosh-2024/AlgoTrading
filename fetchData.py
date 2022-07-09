e3f5af66eb80194f6db96eec2e98dab8b3f43d4f
Ashutosh Aswani <202051215@iiitvadodara.ac.in>
2022-07-09 09:17:30 +0530

    fetchTCSData
    Fetching historical stock market data for TCS for cash market from 2018-01-01 7:00:00 to 2022-07-07 7:00:00.

--- fetchData.py
+++ fetchData.py
@@ -1,1 +1,26 @@
+import pandas as pd
+import json
 
+from breeze_connect import BreezeConnect
+breeze = BreezeConnect(api_key="")
+breeze.generate_session(api_secret="", session_token="")
+
+import datetime
+iso_date_string = datetime.datetime.strptime("21/03/2022","%d/%m/%Y").isoformat()[:10] + 'T05:30:00.000Z'
+iso_date_time_string = datetime.datetime.strptime("21/03/2022 23:59:59","%d/%m/%Y %H:%M:%S").isoformat()[:19] + '.000Z'
+
+historicalData = breeze.get_historical_data(interval="30minute",
+                            from_date= "2018-01-01T07:00:00.000Z",
+                            to_date= "2022-07-07T07:00:00.000Z",
+                            stock_code="TCS",
+                            exchange_code="NSE",
+                            product_type="Cash",
+                            expiry_date="",
+                            right="",
+                            strike_price="")
+
+with open('/Users/ashutoshaswani/Desktop/TcsCashMarket.json', 'w') as json_file:
+    json.dump(historicalData['Success'], json_file)
+    
+df = pd.read_json (r'/Users/ashutoshaswani/Desktop/TcsCashMarket.json')
+df.to_csv (r'/Users/ashutoshaswani/Desktop/TcsCashMarket.csv', index = None)
