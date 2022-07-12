0bd5c073d9cfe215cef7d74b7f667926db0f165a
Ashutosh Aswani <202051215@iiitvadodara.ac.in>
2022-07-12 12:45:03 +0530

    Update placeOrder.py

--- placeOrder.py
+++ placeOrder.py
@@ -10,18 +10,37 @@ breeze.generate_session(api_secret="", session_token="")
 iso_date_string = datetime.datetime.strptime("21/03/2022","%d/%m/%Y").isoformat()[:10] + 'T05:30:00.000Z'
 iso_date_time_string = datetime.datetime.strptime("21/03/2022 23:59:59","%d/%m/%Y %H:%M:%S").isoformat()[:19] + '.000Z'
 
-breeze.place_order(stock_code="TCS",
+# spot price at 12:42 pm on 12th July is at 16101.20
+# placing order for 15900 - 16300 short strangle on 12th July 2022.
+
+breeze.place_order(stock_code="NIFTY",
                     exchange_code="NFO",
-                    product="futures",
-                    action="buy",
+                    product="options",
+                    action="sell",
                     order_type="limit",
-                    stoploss="0",
-                    quantity="3200",
-                    price="3200",
+                    stoploss="50",
+                    quantity="50",
+                    price="36",
                     validity="day",
-                    validity_date="2022-07-08T07:00:00.000Z",
-                    disclosed_quantity="0",
+                    validity_date="2022-07-12T07:00:00.000Z",
+                    disclosed_quantity="50",
                     expiry_date="2022-07-15T06:00:00.000Z",
-                    right="others",
-                    strike_price="0",
+                    right="put",
+                    strike_price="15900",
+                    user_remark="TestOrder")
+
+breeze.place_order(stock_code="NIFTY",
+                    exchange_code="NFO",
+                    product="options",
+                    action="sell",
+                    order_type="limit",
+                    stoploss=40",
+                    quantity="50",
+                    price="27.25",
+                    validity="day",
+                    validity_date="2022-07-12T07:00:00.000Z",
+                    disclosed_quantity="50",
+                    expiry_date="2022-07-15T06:00:00.000Z",
+                    right="call",
+                    strike_price="16300",
                     user_remark="TestOrder")
